8fec6e3472c4f5ae3454444d3ae1b6ac42e78b2a
ShinThant3010 <shinthant3010@gmail.com>
2026-03-04 19:10:51 +0700

    remove 4 replicas test

--- locustfile.py
+++ locustfile.py
@@ -26,11 +26,8 @@ def _load_student_ids_from_csv(csv_path: Path) -> list[str]:
 STUDENT_IDS = _load_student_ids_from_csv(Path("test_data_prep/prep_students/student_ids.csv"))
 RECOMMEND_PATH = "/recommendations"
 
-# LOCUST_VERTEX_INDEX_ENDPOINT = "projects/810737581373/locations/asia-southeast1/indexEndpoints/148170186959093760"
-# LOCUST_VERTEX_DEPLOYED_INDEX_ID = "feeds_20k_deployed"
-
-LOCUST_VERTEX_INDEX_ENDPOINT = "projects/810737581373/locations/asia-southeast1/indexEndpoints/1270410919104479232"
-LOCUST_VERTEX_DEPLOYED_INDEX_ID = "feeds_20k_4replicas_deployed"
+LOCUST_VERTEX_INDEX_ENDPOINT = "projects/810737581373/locations/asia-southeast1/indexEndpoints/148170186959093760"
+LOCUST_VERTEX_DEPLOYED_INDEX_ID = "feeds_20k_deployed"
 
 
 class VertexRequest(BaseModel):
